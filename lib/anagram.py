60201c56fb9add2789e499f6327e15e1352ca4e0
Benhin Mwendwa <benhinmwendwa7@gmail.com>
2024-05-27 16:41:34 +0300

    anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,27 @@
-# your code goes here!
+from collections import Counter
+
+class Anagram:
+    def __init__(self, word):
+        
+        self.word = word.lower()
+        self.word_count = Counter(self.word)
+    
+    def match(self, possible_anagrams):
+        
+        correct_anagrams = []
+        
+        
+        for candidate in possible_anagrams:
+        
+            candidate_count = Counter(candidate.lower())
+            
+            
+            if self.word_count == candidate_count:
+                correct_anagrams.append(candidate)
+        
+        return correct_anagrams
+
+
+listen = Anagram("listen")
+result = listen.match(['enlists', 'google', 'inlets', 'banana'])
+print(result) 
